8e7ff4696e4edb9347aa25378d6972e16747ca81
Nikhil <nikhilphatak1@gmail.com>
2018-10-09 19:34:03 -0400

    Selects number randomly.

--- bot.go
+++ bot.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"fmt"
-	"bufio"
+	//"bufio"
 	"os"
 	"net/http"
 	"log"
 	"io/ioutil"
-	"strings"
-	//"math/rand"
+	//"strings"
+	"math/rand"
 )
 
 
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Give me a number: ")
-	num, _ := reader.ReadString('\n')
-	num = strings.TrimSuffix(num, "\n")
-	url := fmt.Sprintf("http://numbersapi.com/%s/math",num)
+	//reader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Give me a number: ")
+	//num, _ := reader.ReadString('\n')
+	//num = strings.TrimSuffix(num, "\n")
+	num := rand.Intn(100)
+	fmt.Printf("Number selected: %d\n",num)
+	url := fmt.Sprintf("http://numbersapi.com/%d/math",num)
 
 	resp, e := http.Get(url)
 	if e != nil {
-		log.Fatal("GET request: ", e)
+		log.Fatal("Response: ", e)
 		return
 	} else {
 		msg, e := ioutil.ReadAll(resp.Body)
